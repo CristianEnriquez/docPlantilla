6f296413c6853d91f63a5d2f5520fb7fea5cffe5
CristianEnriquez <enriquezer91@gmail.com>
2025-07-28 10:25:12 -0500

    ejemplo remplazo

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
+include_once('tbs_class.php');       // TinyButStrong
+include_once('tbs_plugin_opentbs.php'); // OpenTBS plugin
+
+$TBS = new clsTinyButStrong(); 
+$TBS->Plugin(TBS_INSTALL, OPENTBS_PLUGIN); 
+
 $archivo = 'recursos/ejemplo.docx';
 
-if (file_exists($archivo)) {
-    echo "El documento existe.";
-} else {
-    echo "El documento no existe.";
-}
+$TBS->LoadTemplate($archivo, OPENTBS_ALREADY_UTF8);
+
+$TBS->MergeField('A1', 'Hola Mundo');
+//print_r($TBS);
+
+$TBS->Show(OPENTBS_DOWNLOAD, 'salida.docx');
 
 
 ?>
