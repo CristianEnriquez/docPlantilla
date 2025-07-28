b8d5407c4902896cc428ca5f30646390acab33b7
CristianEnriquez <enriquezer91@gmail.com>
2025-07-28 11:00:46 -0500

    git control plantilla controlada

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ include_once('tbs_plugin_opentbs.php'); // OpenTBS plugin
 $TBS = new clsTinyButStrong(); 
 $TBS->Plugin(TBS_INSTALL, OPENTBS_PLUGIN); 
 
-$archivo = 'recursos/ejemplo.docx';
-//$archivo = 'recursos/plantilla.docx';
+//$archivo = 'recursos/ejemplo.docx';
+$archivo = 'recursos/plantilla.docx';
 
 $TBS->LoadTemplate($archivo, OPENTBS_ALREADY_UTF8);
 
